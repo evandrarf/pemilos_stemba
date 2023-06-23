600468354799098a926007780c3d3beccf308761
Evandra Raditya <evandraraditya049@gmail.com>
2023-06-23 07:29:21 +0700

    feat: ✨ Create new controller and view

---
+++ resources/js/Pages/admin/candidates/index.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    layout: AppLayout,
+};
+</script>
+
+<script setup>
+import { Head } from "@inertiajs/inertia-vue3";
+import { array, object, string } from "vue-types";
+import AppLayout from "@/layouts/apps.vue";
+import VBreadcrumb from "@/components/VBreadCrumb/index.vue";
+
+const props = defineProps({
+    title: string(),
+});
+
+const breadcrumb = [
+    {
+        name: "Dashboard",
+        active: false,
+        to: route("dashboard.index"),
+    },
+    {
+        name: "Candidates",
+        active: true,
+        to: route("candidates.index"),
+    },
+];
+</script>
+
+<template>
+    <Head :title="title" />
+    <VBreadcrumb :routes="breadcrumb" />
+</template>
