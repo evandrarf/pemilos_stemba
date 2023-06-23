fbcc715ffc89e53f85e8a1fb278443611e95f41a
Evandra Raditya <evandraraditya049@gmail.com>
2023-06-23 07:27:47 +0700

    feat: ✨ Create new migrations

---
+++ database/migrations/2023_06_22_172713_create_candidates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('class');
+            $table->string('nis')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates');
+    }
+};
---
+++ database/migrations/2023_06_23_065352_create_candidate_pairs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidate_pairs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('chairman')->unique()->constrained('candidates')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('vice_chairman')->unique()->constrained('candidates')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('image')->nullable();
+            $table->string('vision')->nullable();
+            $table->text('mission')->nullable();
+            $table->integer('number')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates_pair');
+    }
+};
