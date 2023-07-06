11d5e70f04c3f05cc3942b66826e1abf75eba236
Evandra Raditya <evandraraditya049@gmail.com>
2023-07-06 18:11:21 +0700

    feat: ✨ Add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title inertia>{{ config('app.name', '') }}</title>
+
+    <link rel="shortcut icon" href="{{ asset('img/logo-smkn-7-smg-putih.png') }}" type="image/png">
     <!-- Scripts -->
     @routes
-    
+
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     @inertiaHead
-    
+
 </head>
 
 <body class="bg-gray-100 font-sans antialiased">
